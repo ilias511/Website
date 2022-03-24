cb2749960b78f0a2cfbb1c0ac1e8cb02f007c8ed
Hlias Taskoudis <ilias.task@gmail.com>
2022-03-25 01:59:03 +0200

    made everything  clear

--- settings.py
+++ settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'ilias',
+        'PASSWORD': '456456456789',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
