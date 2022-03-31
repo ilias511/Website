32664b8005d8d1524adeeebcc426a1cfd03ea30a
Hlias Taskoudis <ilias.task@gmail.com>
2022-03-31 23:55:15 +0300

    made some tests

--- sites/tests.py
+++ sites/tests.py
@@ -1,15 +1,54 @@
 from django import test as django_test
+from django.urls import reverse
 
-from sites.models import AppUsername
+from sites.models import AppUsername, Post, AppUser
 
 
-class AppUsernameTest(django_test.TestCase):
+#
+# class AppUsernameTest(django_test.TestCase):
+#
+#     def test_true(self):
+#         self.assertTrue(True)
+#
+#     # def test_user_create_when_everything_is_valid(self):
+#     #     user = AppUsername(username='Youngtask' ,age=14)
+#     #     user.save()
+#     #     self.assertIsNotNone(user.pk)
 
-    def test_true(self):
-        self.assertTrue(True)
 
-    # def test_user_create_when_everything_is_valid(self):
-    #     user = AppUsername(username='Youngtask' ,age=14)
-    #     user.save()
-    #     self.assertIsNotNone(user.pk)
+class TestViews(django_test.TestCase):
 
+    def test_project_home(self):
+        client = django_test.Client()
+
+        response = client.get(reverse('home'))
+
+
+        self.assertEquals(response.status_code,200)
+        # self.assertTemplateUsed(response,'base/base.html')
+        self.assertTemplateUsed(response,'home.html')
+
+    def test_project_home_page_with_posts(self):
+        client = django_test.Client()
+
+        user = AppUser.objects.create(
+                email='ilias.task@gmail.com',
+                is_staff=False,
+                is_superuser=False
+
+            )
+        p = Post.objects.create(
+            title='random',
+            details='random 2 test',
+            category='Crypto',
+            user = user
+
+        )
+        p.save()
+
+        response = client.get(reverse('home'))
+
+        posts = response.context['post']
+
+        for p in posts:
+            self.assertIsNotNone(p)
