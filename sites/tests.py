c12f9646f1037be5c006fcbb2c8d13557ffe04c4
Hlias Taskoudis <ilias.task@gmail.com>
2022-04-11 22:40:31 +0300

    finished the tests

--- sites/tests.py
+++ sites/tests.py
@@ -2,7 +2,7 @@ from django import test as django_test
 from django.contrib.auth import login
 from django.urls import reverse
 
-from sites.models import AppUsername, Post, AppUser
+from sites.models import AppUsername, Post, AppUser, Images
 
 
 class TestHomeView(django_test.TestCase):
@@ -123,4 +123,43 @@ class TestUserProfileView(django_test.TestCase):
         self.assertEqual(1,posts)
 
 class TestUserPostsAndImages(django_test.TestCase):
-    pass
+    def test_context_data_of_users_when_have_images_and_posts(self):
+        client = django_test.Client()
+        user = AppUser.objects.create(
+            email='ilias.task@gmail.coma',
+            is_staff=False,
+            is_superuser=False
+
+        )
+        user.set_password('iliasgrbg15')
+        user.save()
+        app_user = AppUsername.objects.create(
+            username='YoungTask',
+            age=16,
+            user=user
+        )
+        client.login(email='ilias.task@gmail.coma', password='iliasgrbg15')
+
+        p = Post.objects.create(
+            title='random',
+            details='random 2 test',
+            category='Crypto',
+            user=user
+
+        )
+        p.save()
+
+        image  = Images.objects.create(
+            title='test image',
+            image= 'https://i0.wp.com/maxblizz.com/wp-content/uploads/2022/02/Doctor-Strange-in-the-Multiverse-of-Madness-1.jpg',
+            user = user
+
+        )
+        response = client.get(reverse('posting'),follow=True)
+        posts = response.context['users_post']
+        images = response.context['users_images']
+        self.assertIsNotNone(posts)
+        self.assertIsNotNone(images)
+        self.assertTemplateUsed(response,'posts.html')
+        self.assertEqual(1,posts.count())
+        self.assertEqual(1,images.count())
